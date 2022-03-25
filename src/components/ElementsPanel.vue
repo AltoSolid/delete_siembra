c8ca73ecdbfc3073fd89c6bf917d75409c4c6051
Santiago <shincapie@playvox.com>
2022-03-25 12:39:56 -0500

    first commit delete_siembra

---
+++ src/components/ElementsPanel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Element
+      v-for="element in plantingInformation"
+      @click.native="deleteElement(element)"
+      :key="element.id"
+      class="center"
+    >
+      Se sembró {{ element.semilla }} el dia {{ element.fechaSiembra.slice(0,10) }}
+    </Element>
+  </div>
+</template>
+
+<script>
+import Element from "./Element";
+
+export default {
+  data() {
+    return {
+      hora: '',
+    }
+  },
+  props: ["plantingInformation"],
+  methods: {
+    deleteElement(plantingInformation, index) {
+      console.log(plantingInformation._id);
+      this.hora = plantingInformation.fechaSiembra;
+      console.log(this.hora);
+      this.$emit("deletedId", plantingInformation._id);
+    },
+  },
+  components: {
+    Element
+  }
+};
+</script>
+
+<style scoped>
+  .center {
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="alert alert-info padding">
+        Info: Click sobre una consulta para eliminarla.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  .padding {
+    margin-top: 10px;
+  }
+</style>
